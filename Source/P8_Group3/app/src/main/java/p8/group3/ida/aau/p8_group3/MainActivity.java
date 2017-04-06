09bf3c93172a9b525a401c7c9aa202f2f20ecc72
Christos Mentzelos <christosmentzelos@h4719.eduroam-5.wlan.aalborguniversitet.dk>
2017-04-06 15:07:38 +0200

    Changes in login pages

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/MainActivity.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/MainActivity.java
@@ -1,5 +1,6 @@
 package p8.group3.ida.aau.p8_group3;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(findViewById(R.id.activity_main), iconFont);
     }
 }
