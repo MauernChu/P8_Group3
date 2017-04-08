88fc67edbeed28b340c4dcd89009159f569cce97
Christos Mentzelos <christosmentzelos@d40a915e.rev.stofanet.dk>
2017-04-08 17:58:35 +0200

    almost complete login page (missing chimp font)

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/MainActivity.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/MainActivity.java
@@ -14,8 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
-        TextView textView = (TextView)findViewById(R.id.usericon);
-        textView.setTypeface(font);
+
 
     }
 }
