45dc389f17c829430dff810753ceabc17f4c6a6a
GremlinBabby <camillamhansen@outlook.com>
2017-04-10 13:37:33 +0200

    Made UI for 'CreateAccount2' and fixed fonts for 1&2

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount.java
@@ -1,9 +1,11 @@
 package p8.group3.ida.aau.p8_group3;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,27 @@ public class CreateAccount extends LoginPage {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
 
+        TextView txchimp = (TextView)findViewById(R.id.loginTitle);
+        Typeface custom_font= Typeface.createFromAsset(getAssets(),"KGCorneroftheSky.ttf");
+        txchimp.setTypeface(custom_font);
+
+        TextView txSocforPa = (TextView)findViewById(R.id.purpose);
+        txSocforPa.setTypeface(custom_font);
+
+        TextView txLogin2 = (TextView)findViewById(R.id.Login2);
+        txLogin2.setTypeface(custom_font);
+
+        TextView txPass = (TextView)findViewById(R.id.Password);
+        txPass.setTypeface(custom_font);
+
+        TextView txRepPass = (TextView)findViewById(R.id.repeat_password);
+        txRepPass.setTypeface(custom_font);
+
+        TextView txEmail = (TextView)findViewById(R.id.email);
+        txEmail.setTypeface(custom_font);
+
+        TextView txNext = (TextView)findViewById(R.id.next);
+        txNext.setTypeface(custom_font);
 
         Button next = (Button) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount2.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount2.java
@@ -1,7 +1,9 @@
 package p8.group3.ida.aau.p8_group3;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by christosmentzelos on 10/04/2017.
@@ -15,7 +17,24 @@ public class CreateAccount2 extends CreateAccount {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account2);
 
+        TextView txchimp = (TextView)findViewById(R.id.loginTitle);
+        Typeface custom_font= Typeface.createFromAsset(getAssets(),"KGCorneroftheSky.ttf");
+        txchimp.setTypeface(custom_font);
 
+        TextView txSocforPa = (TextView)findViewById(R.id.purpose);
+        txSocforPa.setTypeface(custom_font);
+
+        TextView txFinish = (TextView)findViewById(R.id.finish);
+        txFinish.setTypeface(custom_font);
+
+        TextView txCity = (TextView)findViewById(R.id.city);
+        txCity.setTypeface(custom_font);
+
+        TextView txNumberOfChildren = (TextView)findViewById(R.id.numberOfChildren);
+        txNumberOfChildren.setTypeface(custom_font);
+
+        TextView txAgeOfChildren = (TextView)findViewById(R.id.ageOfChildren);
+        txAgeOfChildren.setTypeface(custom_font);
     }
 }
 
