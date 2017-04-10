569de2339874fc43aa130531dfd546357eb7354b
Christos Mentzelos <christosmentzelos@h4719.eduroam-5.wlan.aalborguniversitet.dk>
2017-04-10 11:22:33 +0200

    test

---
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount.java
@@ -0,0 +1,21 @@
+package p8.group3.ida.aau.p8_group3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by christosmentzelos on 10/04/2017.
+ */
+
+public class CreateAccount extends LoginPage {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+
+}
+}
