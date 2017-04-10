dbeeb29e39cbb5c28af2c8a86de59a9a1a2cb1ca
Christos Mentzelos <christosmentzelos@h4719.eduroam-5.wlan.aalborguniversitet.dk>
2017-04-10 09:39:50 +0200

    LoginPage

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/LoginPage.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/LoginPage.java
@@ -3,15 +3,14 @@ package p8.group3.ida.aau.p8_group3;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_page);
 
         TextView txchimp = (TextView)findViewById(R.id.loginTitle);
         Typeface custom_font= Typeface.createFromAsset(getAssets(),"KGCorneroftheSky.ttf");
