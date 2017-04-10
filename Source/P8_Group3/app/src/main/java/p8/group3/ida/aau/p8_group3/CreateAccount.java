bd35b142223f31a5731a757f0d022a6c44a048f0
Christos Mentzelos <christosmentzelos@h4719.eduroam-5.wlan.aalborguniversitet.dk>
2017-04-10 12:02:56 +0200

    For Camilla to check out

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount.java
@@ -1,8 +1,10 @@
 package p8.group3.ida.aau.p8_group3;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
 
 /**
  * Created by christosmentzelos on 10/04/2017.
@@ -16,6 +18,14 @@ public class CreateAccount extends LoginPage {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
 
+
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), CreateAccount2.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
 }
 }
---
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/CreateAccount2.java
@@ -0,0 +1,21 @@
+package p8.group3.ida.aau.p8_group3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by christosmentzelos on 10/04/2017.
+ */
+
+public class CreateAccount2 extends CreateAccount {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account2);
+
+
+    }
+}
+
