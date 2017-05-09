4417776245b1e0bd1279487d5a6746c19500bafe
KLS\karatinka <sakacova.katarina@gmail.com>
2017-05-09 14:52:47 +0200

    added new view - EditProfilePgae and made parcial functionallity for this page

---
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/Presenter/EditProfilePage.java
@@ -0,0 +1,69 @@
+package p8.group3.ida.aau.p8_group3.Presenter;
+
+import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import p8.group3.ida.aau.p8_group3.Database.DAO.ParentDAO;
+import p8.group3.ida.aau.p8_group3.Database.ParentDAOImpl;
+import p8.group3.ida.aau.p8_group3.Model.Parent;
+import p8.group3.ida.aau.p8_group3.R;
+
+/**
+ * Created by karatinka on 9.5.2017.
+ */
+
+public class EditProfilePage extends AppCompatActivity{
+
+    EditText editName;
+    EditText editChildren;
+    EditText editAgeOfChildren;
+    EditText editCity;
+    EditText editAllLanguages;
+    EditText editTextAbout;
+    EditText editTextHobbies;
+
+    private ParentDAO parentDAO;
+
+    private String loginUsername;
+    private String loginPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile_page);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editChildren = (EditText) findViewById(R.id.editChildren);
+        editAgeOfChildren = (EditText) findViewById(R.id.editAge);
+        editCity = (EditText) findViewById(R.id.editCity);
+        editAllLanguages = (EditText) findViewById(R.id.editAllLanguages);
+        editTextAbout = (EditText) findViewById(R.id.editTextAbout);
+        editTextHobbies = (EditText) findViewById(R.id.editTextHobbies);
+
+        parentDAO = new ParentDAOImpl(this);
+        parentDAO.open();
+
+        Bundle bundle = getIntent().getExtras();
+        loginUsername = bundle.getString("loginUsername");
+        loginPassword = bundle.getString("loginPassword");
+
+        Parent editProfileParent = parentDAO.retrieveInformationAboutParent(loginUsername, loginPassword);
+        String profileUserName = editProfileParent.getUsername();
+        int profileChildren = editProfileParent.getNumberOfChildren();
+        String profileAgeOfChildren = editProfileParent.getAgeOfChildren();
+        String profileCity = editProfileParent.getCityOfResidence();
+        //String profileAllLanguages = editProfileParent.getUsername(); we have to add this to a table
+        String profileTextAbout = editProfileParent.getInfoAboutParent();
+        //String profileTextHobbies = editProfileParent.getUsername();
+
+        editName.setText(profileUserName);
+        editChildren.setText(String.valueOf(profileChildren));
+        editAgeOfChildren.setText(profileAgeOfChildren);
+        editCity.setText(profileCity);
+        editTextAbout.setText(profileTextAbout);
+
+    }
+}
