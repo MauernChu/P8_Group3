07e68b9dc7a5120322d1dcd96b5747cf8cb59fbb
Christos Mentzelos <xtosss4@gmail.com>
2017-05-10 14:17:45 +0200

    Average Rating for locations method added. Works, but need to store average scores in table to display them, not 1 variable

--- Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/Database/RatingDAOImpl.java
+++ Source/P8_Group3/app/src/main/java/p8/group3/ida/aau/p8_group3/Database/RatingDAOImpl.java
@@ -27,7 +27,6 @@ public class RatingDAOImpl {
             DatabaseHandler.COLUMN_RATINGPARENTID};
 
 
-
     public RatingDAOImpl(Context context)
     {
         dbHelper = new DatabaseHandler(context);
@@ -54,6 +53,102 @@ public class RatingDAOImpl {
         long insertID = database.insert(DatabaseHandler.TABLE_RATING, null, v);
 
     }
+
+
+    public double getAverageRating (){
+
+        double averageRating = 0;
+
+
+        String query = "SELECT ratingLocationID FROM " + DatabaseHandler.TABLE_RATING;
+
+        Cursor cursor = database.rawQuery(query, null);
+        //cursor.moveToFirst();
+
+        String query1 = "SELECT ratingValue FROM " + DatabaseHandler.TABLE_RATING;
+
+        Cursor cursor1 = database.rawQuery(query1, null);
+        //cursor1.moveToFirst();
+
+        String query2 = "SELECT _locationID FROM " + DatabaseHandler.TABLE_LOCATION;
+
+        Cursor cursor2 = database.rawQuery(query2, null);
+        //cursor2.moveToFirst();
+
+        //System.out.println(cursor.getDouble(1));
+
+
+        int rating_ratingvalue = cursor1.getCount();
+        int ratinglocationid = cursor.getCount();
+        int location_locationid = cursor2.getCount();
+
+        int i;
+        int j;
+
+
+
+        for (i = 0; i < location_locationid; i++){
+
+            int g = 0;
+            averageRating = 0;
+
+
+            String query4 = "SELECT ratingLocationID FROM " + DatabaseHandler.TABLE_RATING;
+
+            Cursor cursor4 = database.rawQuery(query4, null);
+
+
+        //    for (j = 0; j <= ratinglocationid; j++){
+
+                cursor4.moveToFirst();
+
+            String query5 = "SELECT ratingValue FROM " + DatabaseHandler.TABLE_RATING;
+
+            Cursor cursor5 = database.rawQuery(query5, null);
+
+            cursor5.moveToFirst();
+
+                while (cursor4.moveToNext()){
+
+                //cursor.moveToPosition(j);
+
+                if( cursor4.getInt(cursor4.getColumnIndex("ratingLocationID")) == i+1 ){
+
+                    cursor5.moveToPosition(cursor4.getPosition());
+
+                        averageRating = averageRating + cursor5.getInt(cursor5.getColumnIndex("ratingValue"));
+
+
+                    System.out.println(averageRating);
+                        g=g+1;
+
+                } else{
+                    System.out.println("false");
+                }
+
+
+                }
+
+        //}
+
+            if ( g!= 0 ){
+
+                // Na kanw to average rating pinaka wste na apo8hkeuei ton M.O gia to ka8e meros!!
+
+                averageRating = averageRating / g;
+
+            }
+            else {
+                averageRating = 0;
+            }
+
+        }
+
+        System.out.println(averageRating);
+        return averageRating;
+
+    }
+
 
 
     public List<Rating> getMyRating(){
